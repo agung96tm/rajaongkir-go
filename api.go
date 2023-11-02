462bbe00f6d39c1f1c2f3972df2939ed8a1d2e6e
Agung Yuliyanto <agung.96tm@gmail.com>
2023-11-02 21:44:47 +0700

    fix opts in province and city

--- api.go
+++ api.go
@@ -10,10 +10,10 @@ func NewAPI(config Config) API {
 	}
 }
 
-func (a API) GetProvinces(opts ProvinceParams) (ResponseProvinces, error) {
+func (a API) GetProvinces(opts *ProvinceParams) (ResponseProvinces, error) {
 	return get[ResponseProvinces](a.config.BaseUrl, "province", a.config.Header, urlValues(opts))
 }
 
-func (a API) GetCities(opts CityParams) (ResponseCities, error) {
+func (a API) GetCities(opts *CityParams) (ResponseCities, error) {
 	return get[ResponseCities](a.config.BaseUrl, "city", a.config.Header, urlValues(opts))
 }
